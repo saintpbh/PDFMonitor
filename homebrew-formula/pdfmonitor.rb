063239b77dd34490810de14810b272cb68a9071c
BongHwan Park <bongpark@example.com>
2026-05-23 20:22:55 +0900

    chore: Update Homebrew Cask checksum for v0.1.0 release

--- homebrew-formula/pdfmonitor.rb
+++ homebrew-formula/pdfmonitor.rb
@@ -1,6 +1,6 @@
 cask "pdfmonitor" do
   version "0.1.0"
-  sha256 "REPLACE_WITH_SHA256_AFTER_BUILD" # ./package_brew.sh 실행 시 자동 갱신됩니다.
+  sha256 "02b3d180b764858a58428aaff0fe87f84714524a8c4add5771f2f7d8ef0100d2"
 
   url "https://github.com/saintpbh/PDFMonitor/releases/download/v#{version}/PDFMonitor-macOS.zip"
   name "PDFMonitor"
